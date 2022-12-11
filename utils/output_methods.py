e1611250779bbb8ddc82595c0c1886da8cae1522
flashnuke <adanikel@gmail.com>
2022-12-11 15:57:16 +0200

    fix banner print

--- utils/output_methods.py
+++ utils/output_methods.py
@@ -4,6 +4,24 @@ Red = '\033[31m'
 Green = '\033[32m'
 Blue = '\033[34m'
 
+BANNER = f"""
+{Red}██╗  ██╗{White}████████╗{Red}████████╗{White}██████╗         
+{Red}██║  ██║{White}╚══██╔══╝{Red}╚══██╔══╝{White}██╔══██╗        
+{Red}███████║{White}   ██║   {Red}   ██║   {White}██████╔╝        
+{Red}██╔══██║{White}   ██║   {Red}   ██║   {White}██╔═══╝         
+{Red}██║  ██║{White}   ██║   {Red}   ██║   {White}██║             
+{Red}╚═╝  ╚═╝{White}   ╚═╝   {Red}   ╚═╝   {White}╚═╝             
+
+{Red}██████╗ {White}██████╗ {Red}██╗   ██╗{White}████████╗{Red}███████╗{White}
+{Red}██╔══██╗{White}██╔══██╗{Red}██║   ██║{White}╚══██╔══╝{Red}██╔════╝{White}
+{Red}██████╔╝{White}██████╔╝{Red}██║   ██║{White}   ██║   {Red}█████╗  {White}
+{Red}██╔══██╗{White}██╔══██╗{Red}██║   ██║{White}   ██║   {Red}██╔══╝  {White}
+{Red}██████╔╝{White}██║  ██║{Red}╚██████╔╝{White}   ██║   {Red}███████╗{White}
+{Red}╚═════╝ {White}╚═╝  ╚═╝{Red} ╚═════╝ {White}   ╚═╝   {Red}╚══════╝{White}
+
+Written by @flashnuke
+"""
+
 
 def print_error(text: str):
     print(f"[{Bold}{Red}!{White}] {text}")
@@ -18,20 +36,4 @@ def print_info(text: str):
 
 
 def print_banner():
-    print(f"""
-          {Red}██╗  ██╗{White}████████╗{Red}████████╗{White}██████╗         
-          {Red}██║  ██║{White}╚══██╔══╝{Red}╚══██╔══╝{White}██╔══██╗        
-          {Red}███████║{White}   ██║   {Red}   ██║   {White}██████╔╝        
-          {Red}██╔══██║{White}   ██║   {Red}   ██║   {White}██╔═══╝         
-          {Red}██║  ██║{White}   ██║   {Red}   ██║   {White}██║             
-          {Red}╚═╝  ╚═╝{White}   ╚═╝   {Red}   ╚═╝   {White}╚═╝             
-          
-          {Red}██████╗ {White}██████╗ {Red}██╗   ██╗{White}████████╗{Red}███████╗{White}
-          {Red}██╔══██╗{White}██╔══██╗{Red}██║   ██║{White}╚══██╔══╝{Red}██╔════╝{White}
-          {Red}██████╔╝{White}██████╔╝{Red}██║   ██║{White}   ██║   {Red}█████╗  {White}
-          {Red}██╔══██╗{White}██╔══██╗{Red}██║   ██║{White}   ██║   {Red}██╔══╝  {White}
-          {Red}██████╔╝{White}██║  ██║{Red}╚██████╔╝{White}   ██║   {Red}███████╗{White}
-          {Red}╚═════╝ {White}╚═╝  ╚═╝{Red} ╚═════╝ {White}   ╚═╝   {Red}╚══════╝{White}
-          
-          Written by @flashnuke
-          """)
+    print(BANNER)
