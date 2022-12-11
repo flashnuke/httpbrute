81cee148d393039a98ea77f37978b667c8883e3c
flashnuke <adanikel@gmail.com>
2022-12-11 15:30:12 +0200

    better prints

--- httpbrute.py
+++ httpbrute.py
@@ -12,6 +12,7 @@ from urllib3.exceptions import HTTPError
 
 class HTTPBrute:
     _SUCCESS_SCODE = 200
+    _LOG_STATUS_INTV = 30  # in seconds
 
     def __init__(self,
                  target_url: str,
@@ -27,6 +28,9 @@ class HTTPBrute:
             self._terminate(f"username list [size {len(self._usernames)}] "
                             f"and password list [size {self._passwords_queue.qsize()}] "
                             f"cannot be empty!")
+        else:
+            print_success(f"loaded {len(self._usernames)} usernames and "
+                          f"{self._passwords_queue.qsize()} passwords")
 
         self._url = target_url
         self._session = requests.Session()
@@ -37,6 +41,8 @@ class HTTPBrute:
 
         self._start = float()
         self._finished = False
+        self._last_status_log = float()
+        self._results = dict()
 
     def _make_request(self, *args, **kwargs):
         """
@@ -58,14 +64,14 @@ class HTTPBrute:
             try:
                 response = self._make_request(auth=auth)
                 if response.status_code == HTTPBrute._SUCCESS_SCODE:
-                    print(f"[+] password found -> {passw}")
                     self.finished = True
-                sz = self._passwords_queue.qsize()
-                if sz % 10000 == 0 and not self._finished:
-                    print(f"[*] left -> {sz} | elapsed time -> {time.time() - self._start}[s]")
+                now = time.time()
+                if now - self._last_status_log > HTTPBrute._LOG_STATUS_INTV and not self._finished:
+                    print_info(f"passwords left -> {self._passwords_queue.qsize()} | "
+                               f"elapsed time -> {round(time.time() - self._start, 2)}[s]")
             except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout,
                     requests.exceptions.ReadTimeout, HTTPError):
-                print(f"[!] connection timeout for password '{passw}', putting back in queue...")
+                print_error(f"connection timeout for password '{passw}', putting back in queue...")
                 self._passwords_queue.put(passw)
                 continue
             except requests.exceptions.TooManyRedirects:
@@ -75,6 +81,10 @@ class HTTPBrute:
             finally:
                 if self._sleep_intv:  # don't call `sleep()` if interval==0 to avoid context switch overhead
                     time.sleep(self._sleep_intv)
+
+    def log_success(self, user: str, passwd: str):
+        self._finished = True
+        self._results[user] = passwd
 
     def _get_auth_type(self) -> Union[Type[HTTPBasicAuth], Type[HTTPDigestAuth]]:
         try:
@@ -86,10 +96,10 @@ class HTTPBrute:
                 elif 'digest' in auth_header.lower():
                     return HTTPDigestAuth
                 else:
-                    print(f"[!] auth header '{auth_header}' is not of type basic / digest!")
+                    print_error(f"auth header '{auth_header}' is not of type basic / digest!")
                     self._terminate(f"unsupported auth-type: {auth_header}")
             else:
-                print(f"[!] auth header is missing, please check the url")
+                print_error(f"auth header is missing, please check the url")
                 self._terminate(f"missing auth header ('WWW-Authenticate')")
         except Exception as exc:
             self._terminate(f"unable to determine auth-type, exception - {exc}")
@@ -97,7 +107,7 @@ class HTTPBrute:
     def run(self):
         try:
             for user in self._usernames:
-                print(f"[*] setting up {self._workers_count} workers for username -> {user}")
+                print_info(f"setting up {self._workers_count} workers for username -> {user}")
                 threads = list()
                 self._start = time.time()
                 for passw in range(self._workers_count):
@@ -107,6 +117,10 @@ class HTTPBrute:
 
                 for worker in threads:
                     worker.join()
+                if user in self._results:
+                    print_success(f"username {user} found password -> {self._results[user]}")
+                else:
+                    print_success(f"username {user} found failed to find password")
         except KeyboardInterrupt:
             self._terminate("user request")
         except Exception as exc:
@@ -117,7 +131,7 @@ class HTTPBrute:
         """
         happens on error / user interrupt
         """
-        print(f"[!] terminating, reason -> {reason}")
+        print_error(f"terminating, reason -> {reason}")
         os.kill(os.getpid(), 9)
 
 
@@ -126,12 +140,12 @@ if __name__ == "__main__":
     arguments = parser.parse_args()
 
     word_lists = dict()
-    for args in [("user", "user_list"), ("pass", "pass_list")]:
-        single_word, wordlist_path = tuple(getattr(arguments, arg) for arg in args)
+    for l_args in [("user", "user_list"), ("pass", "pass_list")]:
+        single_word, wordlist_path = tuple(getattr(arguments, arg) for arg in l_args)
         if not has_one([single_word, wordlist_path]):
-            print(f"Exactly one of the following args should be used -> {args}")
+            print(f"Exactly one of the following args should be used -> {l_args}")
             exit(-1)
-        word_lists[args[1]] = load_wordlist(wordlist_path) if wordlist_path else [single_word]
+        word_lists[l_args[1]] = load_wordlist(wordlist_path) if wordlist_path else [single_word]
 
     HTTPBrute(target_url=arguments.target_url,
               workers_count=arguments.workers,
---
+++ utils/output_methods.py
@@ -0,0 +1,17 @@
+White = '\033[0m'
+Bold = '\033[1m'
+Red = '\033[31m'
+Green = '\033[32m'
+Blue = '\033[34m'
+
+
+def print_error(text: str):
+    print(f"[{Bold}{Red}!{White}] {text}")
+
+
+def print_success(text: str):
+    print(f"[{Bold}{Green}+{White}] {text}")
+
+
+def print_info(text: str):
+    print(f"[{Bold}{Blue}*{White}] {text}")
